520fc1c30f43ac2a8758865082513d3ab53812c2
jay-kishan010 <124438245+jay-kishan010@users.noreply.github.com>
2025-07-13 13:29:50 +0530

    ..

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -18,7 +18,7 @@ const Contact = () => {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://localhost:5000/api/contact", {
+      const res = await fetch("https://mannka-coming-soon-ba.onrender.com/api/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
