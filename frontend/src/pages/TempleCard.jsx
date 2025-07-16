137ef16a8db9dd0f1582f1d58e7787aa7b2ea6b1
HARSHIKA-1508 <harshikas898@gmail.com>
2025-07-17 01:02:25 +0530

    TempleCard.jsx

---
+++ frontend/src/pages/TempleCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './TempleCard.css';
+
+const TempleCard = ({ title, imgSrc, brief, contact }) => {
+  return (
+    <div className="temple-card">
+      <img src={imgSrc} alt={title} className="temple-image" />
+      <div className="temple-info">
+        <h3>{title}</h3>
+        <p>{brief}</p>
+        <a href={`tel:${contact}`} className="book-btn">Call to Book: {contact}</a>
+      </div>
+    </div>
+  );
+};
+
+export default TempleCard;
