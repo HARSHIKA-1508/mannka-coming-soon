35d8425c508c304b4d898e649e2609301e98fc90
HARSHIKA-1508 <harshikas898@gmail.com>
2025-07-17 01:00:47 +0530

    Hero.jsx

---
+++ frontend/src/pages/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Hero.css';
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <h1>On the occasion of Savan</h1>
+      <h2>Providing Special Darshan</h2>
+    </div>
+  );
+};
+
+export default Hero;
