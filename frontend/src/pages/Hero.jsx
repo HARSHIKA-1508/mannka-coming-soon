b473fe0948337dc5cb58be65ef907964c57bb853
HARSHIKA-1508 <harshikas898@gmail.com>
2025-07-17 03:21:15 +0530

    Hero.jsx

--- frontend/src/pages/Hero.jsx
+++ frontend/src/pages/Hero.jsx
@@ -4,7 +4,7 @@ import './Hero.css';
 const Hero = () => {
   return (
     <div className="hero">
-      <h1>On the occasion of Savan</h1>
+      <h1>On The Occasion Of Savan</h1>
       <h2>Providing Special Darshan</h2>
     </div>
   );
