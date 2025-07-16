1d9fca3ffd0a7a799ac3ee6c8219ce168a4850c6
HARSHIKA-1508 <harshikas898@gmail.com>
2025-07-17 00:56:37 +0530

    Navbar.jsx

---
+++ frontend/src/pages/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Navbar.css';
+import logo from '../assets/logo.png';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-left">
+        <img src={logo} alt="Company Logo" className="logo" />
+        <div className="company-info">
+          <h2 className="company-name">TempleWalk</h2>
+          <p className="tagline">Giving Local Experience</p>
+        </div>
+      </div>
+
+      <div className="navbar-right">
+        <select className="language-select">
+          <option>EN</option>
+          <option>HI</option>
+        </select>
+        <a href="tel:+919999999999" className="call-icon" title="Call Us">📞</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
