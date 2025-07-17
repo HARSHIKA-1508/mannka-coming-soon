0ef869c4a6a15c28c37b6c3c45d1cf270b61efbc
HARSHIKA-1508 <harshikas898@gmail.com>
2025-07-18 00:46:56 +0530

    Navbar.jsx

--- frontend/src/pages/Navbar.jsx
+++ frontend/src/pages/Navbar.jsx
@@ -6,24 +6,19 @@ import { faLanguage, faPhone } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
   useEffect(() => {
-    // Prevent re-adding script if already present
-    if (!document.getElementById('google-translate-script')) {
-      const script = document.createElement("script");
-      script.id = 'google-translate-script';
-      script.src = "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
-      script.async = true;
-      document.body.appendChild(script);
+    const script = document.createElement("script");
+    script.src = "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+    script.async = true;
+    document.body.appendChild(script);
 
-      window.googleTranslateElementInit = () => {
-        if (!document.querySelector('#google_translate_element select')) {
-          new window.google.translate.TranslateElement({
-            pageLanguage: 'en',
-            includedLanguages: 'en,hi,gu,ta,te,bn,mr,pa',
-            layout: window.google.translate.TranslateElement.InlineLayout.SIMPLE
-          }, 'google_translate_element');
-        }
-      };
-    }
+    window.googleTranslateElementInit = () => {
+      if (!document.getElementById('google_translate_element')) return;
+      new window.google.translate.TranslateElement({
+        pageLanguage: 'en',
+        includedLanguages: 'en,hi,gu,ta,te,bn,mr,pa',
+        layout: window.google.translate.TranslateElement.InlineLayout.SIMPLE
+      }, 'google_translate_element');
+    };
   }, []);
 
   return (
@@ -32,15 +27,18 @@ const Navbar = () => {
         <img src={logo} alt="Company Logo" className="logo" />
         <div className="company-info">
           <h2 className="company-name">Mannka</h2>
-          <p className="tagline">Providing The Local Experience To Our Atithi</p>
+          <p className="tagline">Giving Local Experience</p>
         </div>
       </div>
 
       <div className="navbar-right">
-        <div className="translate-container" title="Translate" onClick={handleTranslateClick}>
-  <FontAwesomeIcon icon={faLanguage} className="translate-icon" />
-</div>
+        {/* Language Translate with Icon */}
+        <div className="translate-container">
+          <FontAwesomeIcon icon={faLanguage} className="translate-icon" />
+          <div id="google_translate_element"></div>
+        </div>
 
+        {/* Font Awesome Phone Icon */}
         <a href="tel:+918858855308" className="call-icon" title="Call Us">
           <FontAwesomeIcon icon={faPhone} />
         </a>
